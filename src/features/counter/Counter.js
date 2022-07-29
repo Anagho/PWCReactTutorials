16358286ff89c64e0fc7c0c223223f6d5bb97e69
Anagho okiroro Obruthe <anaghosamroyce@gmail.com>
2022-07-29 04:02:02 +0100

    Added CSS to Counter

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,16 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { increment, decrement } from "./counterSlice";
+import { increment, decrement, reset, incrementByAmount, decrementByAmount } from "./counterSlice";
+import Header from '../../components/Header';
+import Card from '../../components/shared/Card';
+import { keyboard } from '@testing-library/user-event/dist/keyboard';
 
 const Counter = () => {
     const count = useSelector((state) => state.counter.count);
     const dispatch = useDispatch();
+    const [amount, setAmount] = useState(0);
+    const [reduceAmount, setReduceAmount] = useState(0);
+
     return (
         <div>
-            <h2>Counter</h2>
-            <p>{count}</p>
-            <button onClick={() => dispatch(increment())}>+</button>
-            <button onClick={() => dispatch(decrement())}>-</button>
+            <Header />
+            <div className='count-wrapper'>
+                <p className='count'>{count}</p>
+            </div>
+            <div className='container'>
+                <button className='btn btn-primary' onClick={() => dispatch(increment())}>+</button>
+                <button className='btn btn-secondary' onClick={() => dispatch(decrement())}>-</button>
+                <button className='btn btn-reset' onClick={() => dispatch(reset(), setAmount(0), setReduceAmount(0))}>Reset</button>
+
+
+                <div className='amount-card'>
+                    <div className='amount-plus'>
+                        <h3>Increment By Amount</h3>
+                        <input placeholder='0' value={amount} onChange={(e) => setAmount(e.target.value)} />
+                        <button className='btn-primary btn-inc' onClick={() => dispatch(incrementByAmount(Number(amount)))}>Add Amount</button>
+                    </div>
+                    <div className='line'></div>
+                    <div className='amount-minus'>
+                        <h3>Decrement By Amount</h3>
+                        <input placeholder='0' value={reduceAmount} onChange={(e) => setReduceAmount(e.target.value)} />
+                        <button className='btn-secondary btn-dec' onClick={() => dispatch(decrementByAmount(Number(reduceAmount)))}>Reduce Amount</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/features/counter/Display.js
+++ src/features/counter/Display.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import { useSelector } from "react-redux"
+import Card from '../../components/shared/Card'
 
 const Display = () => {
     const count = useSelector((state) => state.counter.count)
     return (
-        <div>
-            <h3>Display</h3>
-            <h1>{count}</h1>
+        <div className='container'>
+
+            <div className='display'>
+                <Card>
+                    <h3>Display</h3>
+                    <h1 className='count-text'>{count}</h1>
+                </Card>
+            </div>
+
         </div>
 
     )
